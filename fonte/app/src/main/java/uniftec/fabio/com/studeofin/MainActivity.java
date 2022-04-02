f5a22d65631e11709df20abcef8a52e61357e19c
fabio.dietrich <fabio.dietrich@hotmail.com>
2022-04-02 20:45:00 -0300

    layout menu flutuante

--- fonte/app/src/main/java/uniftec/fabio/com/studeofin/MainActivity.java
+++ fonte/app/src/main/java/uniftec/fabio/com/studeofin/MainActivity.java
@@ -1,18 +1,27 @@
 package uniftec.fabio.com.studeofin;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 import android.os.Bundle;
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_main);
-        setContentView(R.layout.login);
-
-
-
+        setContentView(R.layout.principal);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.open_drawer, R.string.close_drawer);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
     }
 }
+
