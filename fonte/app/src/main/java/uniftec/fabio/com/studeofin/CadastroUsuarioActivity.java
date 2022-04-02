9597f548072841a797569d599120ec57a89f01a2
fabio.dietrich <fabio.dietrich@hotmail.com>
2022-04-02 11:34:49 -0300

    alterações nas telas

--- fonte/app/src/main/java/uniftec/fabio/com/studeofin/CadastroUsuarioActivity.java
+++ fonte/app/src/main/java/uniftec/fabio/com/studeofin/CadastroUsuarioActivity.java
@@ -1,16 +1,15 @@
 package uniftec.fabio.com.studeofin;
 
+import androidx.appcompat.app.AppCompatActivity;
+import de.hdodenhof.circleimageview.CircleImageView;
 import android.os.Bundle;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-public class PrincipalActivity extends AppCompatActivity {
+public class CadastroUsuarioActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login);
-
-
+        setContentView(R.layout.cadastro_usuario);
     }
+
 }
--- fonte/app/src/main/java/uniftec/fabio/com/studeofin/CancelarConta.java
+++ fonte/app/src/main/java/uniftec/fabio/com/studeofin/CancelarConta.java
@@ -1,16 +1,14 @@
 package uniftec.fabio.com.studeofin;
 
-import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
 
-public class PrincipalActivity extends AppCompatActivity {
+import android.os.Bundle;
+
+public class CancelarConta extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login);
-
-
+        setContentView(R.layout.cancelar_conta);
     }
 }
