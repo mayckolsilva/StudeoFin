81f16b7540f2610a86a224c8e3c3b1700914a08b
DiegoH <diegohnck@hotmail.com>
2022-04-17 23:45:42 -0300

    Alterações DH

--- fonte/app/src/main/java/uniftec/fabio/com/studeofin/fragment/CategoriasFragment.java
+++ fonte/app/src/main/java/uniftec/fabio/com/studeofin/fragment/CategoriasFragment.java
@@ -3,6 +3,7 @@ package uniftec.fabio.com.studeofin.fragment;
 import static android.content.Context.MODE_PRIVATE;
 import static android.database.sqlite.SQLiteDatabase.openOrCreateDatabase;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
@@ -10,13 +11,15 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ListView;
+import android.widget.AdapterView;
 
 import java.util.ArrayList;
 
+import uniftec.fabio.com.studeofin.R;
 import uniftec.fabio.com.studeofin.adapter.CategoriasAdapter;
 import uniftec.fabio.com.studeofin.databinding.FragmentCategoriasBinding;
 import uniftec.fabio.com.studeofin.global.Global;
@@ -27,9 +30,8 @@ public class CategoriasFragment extends Fragment {
     FragmentCategoriasBinding binding;
     private SQLiteDatabase query;
     ArrayList<CategoriasVO> categorias;
+    private CategoriasVO catSelecionada;
     private Integer indTipo;
-    ListView lv;
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -40,12 +42,11 @@ public class CategoriasFragment extends Fragment {
         binding.btnAddCategoria.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-
-
+                catSelecionada = new CategoriasVO();
+                binding.edtDescCategoria.setText(null);
+                binding.rdgCategoriaTipo.clearCheck();
             }
         });
-
 
         binding.btnSalvarCategoria.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -56,13 +57,18 @@ public class CategoriasFragment extends Fragment {
                     query = getActivity().openOrCreateDatabase("studeofin", android.content.Context.MODE_PRIVATE,
                             null);
 
-                    String sql = "INSERT INTO categorias (des_categoria, ind_tipo_categoria, id_usuario) VALUES " +
-                            "( '" + binding.edtDescCategoria.getText().toString().trim() + "'," + indTipo + ","+ Global.getIdUsuario() + ")";
-                    query.execSQL("INSERT INTO categorias (des_categoria, ind_tipo_categoria, id_usuario) VALUES " +
-                            "( '" + binding.edtDescCategoria.getText().toString().trim() + "'," + indTipo + ","+ Global.getIdUsuario() + ")");
+                    if(catSelecionada.getCodCategoria()!=null){
+
+                        ContentValues values = new ContentValues();
+                        values.put("des_categoria", binding.edtDescCategoria.getText().toString().trim());
+                        values.put("ind_tipo_categoria", indTipo);
+                        query.update("categorias", values,"id_categoria = ?", new String[]{String.valueOf(catSelecionada.getCodCategoria())} );
+                    } else {
+                        query.execSQL("INSERT INTO categorias (des_categoria, ind_tipo_categoria, id_usuario) VALUES " +
+                                "( '" + binding.edtDescCategoria.getText().toString().trim() + "'," + indTipo + ","+ Global.getIdUsuario() + ")");
+
+                    }
                     buscaCategorias();
-
-
                 }
             }
         });
@@ -70,8 +76,8 @@ public class CategoriasFragment extends Fragment {
         binding.btnExcluirCategoria.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-
+                query.delete("categorias","id_categoria = ?", new String[]{String.valueOf(catSelecionada.getCodCategoria())});
+                buscaCategorias();
             }
         });
 
@@ -95,15 +101,16 @@ public class CategoriasFragment extends Fragment {
                 indTipo = 2;
             }
         });
+
         buscaCategorias();
 
-        if(categorias!=null){
-            binding.listaCategorias.setAdapter(new CategoriasAdapter(getActivity(),categorias));
-        }
-
-
+        binding.listaCategorias.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                clickCategoria(categorias.get(i));
+            }
+        });
         return root;
-
     }
 
     private boolean verificaCampos(){
@@ -115,9 +122,6 @@ public class CategoriasFragment extends Fragment {
         } else {
             return true;
         }
-
-
-
     }
 
     private void buscaCategorias(){
@@ -133,52 +137,44 @@ public class CategoriasFragment extends Fragment {
             int indiceDesCategoria = busca.getColumnIndex("des_categoria");
             int indiceIndTipo= busca.getColumnIndex("ind_tipo_categoria");
             int indiceIdMeta = busca.getColumnIndex("id_meta");
+
+            categorias = new ArrayList<CategoriasVO>();
             busca.moveToFirst();
             if(busca.getCount()>0){
-                CategoriasVO categoria = new CategoriasVO();
-                categoria.setCodCategoria(busca.getInt(0));
-                categoria.setDesCategoria(busca.getString(1));
-                categoria.setIndTipo(busca.getInt(2));
-                categoria.setIdMeta(busca.getInt(3));
-
-                this.getCategorias().add(categoria);
+                while(!busca.isAfterLast()){
+                    CategoriasVO categoria = new CategoriasVO();
+                    categoria.setCodCategoria(busca.getInt(0));
+                    categoria.setDesCategoria(busca.getString(1));
+                    categoria.setIndTipo(busca.getInt(2));
+                    categoria.setIdMeta(busca.getInt(3));
+                    this.getCategorias().add(categoria);
+                    busca.moveToNext();
+                }
             }
 
-
-
+            if(categorias!=null){
+                binding.listaCategorias.setAdapter(new CategoriasAdapter(getActivity(),categorias));
+            }
         } catch(Exception e){
-
-
-
+            Log.println(Log.ERROR,"Categoria","Erro na busca da categoria" );
         }
-
-
     }
 
-    public void clickCategoria(final CategoriasVO categoria) {
+    private void clickCategoria(final CategoriasVO categoria) {
 
-        binding.txtDescCategoria.setText(categoria.getDesCategoria());
+        binding.edtDescCategoria.setText(categoria.getDesCategoria());
 
+        catSelecionada = categoria;
         if(categoria.getIndTipo() == 0){
-            binding.rdbReceita.setSelected(true);
-            binding.rdbDespesa.setSelected(false);
-            binding.rdbMeta.setSelected(false);
-
+            binding.rdgCategoriaTipo.check(R.id.rdb_receita);
+            indTipo = 0;
         } else if (categoria.getIndTipo() == 1) {
-            binding.rdbReceita.setSelected(false);
-            binding.rdbDespesa.setSelected(true);
-            binding.rdbMeta.setSelected(false);
-
+            binding.rdgCategoriaTipo.check(R.id.rdb_despesa);
+            indTipo=1;
         } else {
-            binding.rdbReceita.setSelected(false);
-            binding.rdbDespesa.setSelected(false);
-            binding.rdbMeta.setSelected(true);
+            binding.rdgCategoriaTipo.check(R.id.rdb_meta);
+            indTipo=2;
         }
-
-
-
-
-
     }
 
     public ArrayList<CategoriasVO> getCategorias() {
