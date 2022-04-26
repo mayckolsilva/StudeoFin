ccb1d24a6742849af41cc677fc67c9929cece068
mayckolsilva <mowsilva@gmail.com>
2022-04-25 23:42:00 -0300

    Adicionado os links para acesso às suas devidas páginas na internet.

--- fonte/app/src/main/java/uniftec/fabio/com/studeofin/fragment/DicasInvestimentoFragment.java
+++ fonte/app/src/main/java/uniftec/fabio/com/studeofin/fragment/DicasInvestimentoFragment.java
@@ -1,5 +1,7 @@
 package uniftec.fabio.com.studeofin.fragment;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,8 +10,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import uniftec.fabio.com.studeofin.R;
-import uniftec.fabio.com.studeofin.databinding.FragmentControleSaldoBinding;
+
 import uniftec.fabio.com.studeofin.databinding.FragmentDicasInvestimentoBinding;
 
 
@@ -24,8 +25,55 @@ public class DicasInvestimentoFragment extends Fragment {
         View root = binding.getRoot();
 
 
+
+        binding.dica1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String link = "https://economia.uol.com.br/";
+                Intent acesso = new Intent(Intent.ACTION_VIEW, Uri.parse(link));
+                startActivity(acesso);
+            }
+        });
+        binding.dica2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String link = "https://mepoupe.com/";
+                Intent acesso = new Intent(Intent.ACTION_VIEW, Uri.parse(link));
+                startActivity(acesso);
+            }
+        });
+        binding.dica3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String link = "https://www.oprimorico.com.br/";
+                Intent acesso = new Intent(Intent.ACTION_VIEW, Uri.parse(link));
+                startActivity(acesso);
+            }
+        });
+        binding.dica4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String link = "https://investidorsardinha.r7.com/";
+                Intent acesso = new Intent(Intent.ACTION_VIEW, Uri.parse(link));
+                startActivity(acesso);
+            }
+        });
+        binding.dica5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String link = "https://fontedafortuna.com/";
+                Intent acesso = new Intent(Intent.ACTION_VIEW, Uri.parse(link));
+                startActivity(acesso);
+            }
+        });
+        binding.dica6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String link = "https://hackeandofinancas.com.br/";
+                Intent acesso = new Intent(Intent.ACTION_VIEW, Uri.parse(link));
+                startActivity(acesso);
+            }
+        });
         return root;
-
-
     }
 }
